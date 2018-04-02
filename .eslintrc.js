90b03a1dfc4ccb9e8b5657fbb3c0aa9e135dbef2
ahmadghizzawi <ahmadghizzawi@gmail.com>
2018-04-02 20:58:23 +0100

    t

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+      "node": true,
+      "es6": true
+    },
+    "parserOptions": {
+      "ecmaVersion": 6,
+      "sourceType": "module",
+    },
+    "rules": {
+      // Additional, per-project rules...
+    }
+};
